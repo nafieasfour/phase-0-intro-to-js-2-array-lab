254ee412c949221212d9abb8949440ab683a5b72
Nafie Asfour <ph.nafee.asfour@gmail.com>
2023-06-03 21:26:57 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+
+
+function destructivelyAppendCat(name){
+    return cats.push(name)
+}
+destructivelyAppendCat(Ralph)
+
+
+function destructivelyPrependCat(cat){
+    return cats.unshift(cat)
+}
+function destructivelyRemoveLastCat(name){
+    return cats.pop()
+}
+
+function destructivelyRemoveFirstCat(){
+    return cats.shift()
+}
+
+function appendCat(name){
+    let newcats;
+    return  newcats = [...cats, name]
+}
+
+function prependCat(name){
+    let newcats;
+    return  newcats = [name, ...cats]
+}
+function removeLastCat(){
+    let new_cats = [...cats]
+    new_cats.pop()
+    return new_cats
+}
+function removeFirstCat(){
+    let new_cats = [...cats]
+    new_cats.shift()
+    return new_cats
+}
